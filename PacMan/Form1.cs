e19d1c0be4a9ec216609c3c94b4f3733cc711aff
Panzerfahrer1 <oliverangerer0@gmail.com>
2025-12-26 12:33:07 +0100

    idk what i changed sorry

--- PacMan/Form1.cs
+++ PacMan/Form1.cs
@@ -1,5 +1,6 @@
 using Pacman.GameLogic;
 using PacMan.Classes;
+using System.Windows.Forms;
 
 namespace Pacman
 {
@@ -14,7 +15,7 @@ namespace Pacman
         {
 
             Field[,] gameField = InitGameField();
-            
+
             Player player = new Player(5, 5, 5);
             GameManager gameManager = new GameManager(gameField, 10, player);
 
@@ -22,10 +23,12 @@ namespace Pacman
 
             for (int i = 0; i < 10; i++)
             {
-                gameManager.Update();
+                //gameManager.Update();
             }
 
             MessageBox.Show($"Player new position");
+
+            InitializeGameFieldUI(gameManager);
         }
 
         private Field[,] InitGameField()
@@ -47,5 +50,61 @@ namespace Pacman
             }
             return gameField;
         }
+
+        private void InitializeGameFieldUI(GameManager game)
+        {
+            var gameFieldPanel = new TableLayoutPanel
+            {
+                Name = "gameFieldPanel",
+                RowCount = game.GameField.GetLength(1),
+                ColumnCount = game.GameField.GetLength(0),
+                Dock = DockStyle.Fill,
+                BackColor = Color.DarkGray,
+                Margin = Padding.Empty,
+                Padding = Padding.Empty
+            };
+
+            gameFieldPanel.SuspendLayout();
+            gameFieldPanel.RowStyles.Clear();
+            gameFieldPanel.ColumnStyles.Clear();
+
+            // Wichtig: Prozent-Styles, sonst können Rows/Cols 0 groß sein
+            for (int y = 0; y < gameFieldPanel.RowCount; y++)
+                gameFieldPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 100f / gameFieldPanel.RowCount));
+
+            for (int x = 0; x < gameFieldPanel.ColumnCount; x++)
+                gameFieldPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100f / gameFieldPanel.ColumnCount));
+
+            for (int y = 0; y < gameFieldPanel.RowCount; y++)
+            {
+                for (int x = 0; x < gameFieldPanel.ColumnCount; x++)
+                {
+                    Image img = null;
+                    if(game.GameField[x, y].Type == FieldType.Pellet)
+                    {
+                        img = Image.FromFile(@"C:/Users/olive/Downloads/ER5hyI3W4AwxGEG.png");
+                    }
+
+                    var panel = GameRenderer.CreatePictureBox(
+                        x,
+                        y,
+                        //game.GameField[x, y].Type == FieldType.Wall ? Color.Blue : Color.Black,
+                        img
+                    );
+
+                    gameFieldPanel.Controls.Add(panel, x, y);
+                }
+            }
+
+            // ALTES entfernen, falls du es mehrfach aufrufst
+            var old = this.Controls["gameFieldPanel"];
+            if (old != null) this.Controls.Remove(old);
+
+            this.Controls.Add(gameFieldPanel); // <-- DAS hast du vergessen
+            gameFieldPanel.BringToFront();
+
+            gameFieldPanel.ResumeLayout();
+        }
+
     }
 }
---
+++ PacMan/GameRenderer.cs
@@ -0,0 +1,40 @@
+﻿using PacMan.Classes;
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Pacman
+{
+    public static class GameRenderer
+    {
+
+        public static Panel CreatePanel(int x, int y, Color backColor, Image? image = null)
+        {
+            var panel = new Panel
+            {
+                Name = $"panel_{x}_{y}",
+                Dock = DockStyle.Fill,
+                Margin = Padding.Empty,
+                BackColor = backColor,
+                BackgroundImage = image
+            };
+
+            return panel;
+        }
+
+        public static PictureBox CreatePictureBox(int x, int y, Image? image = null)
+        {
+            var pictureBox = new PictureBox
+            {
+                Name = $"pictureBox_{x}_{y}",
+                Dock = DockStyle.Fill,
+                Margin = Padding.Empty,
+                BackColor = Color.Transparent,
+                Image = image,
+                SizeMode = PictureBoxSizeMode.StretchImage
+            };
+            return pictureBox;
+        }
+    }
+}
